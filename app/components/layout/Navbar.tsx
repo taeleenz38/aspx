fc4d31e0fcb70eef9d7536ecde41ff8e34c3d3db
Tae Kyu Lee <taekyu.lee3@gmail.com>
2025-06-19 21:58:04 +1000

    ui

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -6,8 +6,8 @@ import HamburgerToggle from "../ui/HamburgerToggle";
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 left-0 right-0 pt-4 px-4 z-50 flex justify-between text-primary items-center h-28 bg-white">
-      <Image src="/logo.png" width={125} height={125} alt="logo" />
+    <div className="fixed top-0 left-0 right-0 py-4 px-4 z-50 flex justify-between text-primary items-center h-32 bg-white">
+      <Image src="/logo.png" width={110} height={110} alt="logo" />
       <div className="flex items-center gap-14 text-3xl pr-16 font-extralight">
         <Link
           href="/"
