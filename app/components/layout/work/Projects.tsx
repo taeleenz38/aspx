aefb38b9694397e81f43f9838717558f945e6869
Tae Kyu Lee <taekyu.lee3@gmail.com>
2025-06-21 02:37:41 +1000

    project images - gradient layover

--- app/components/layout/work/Projects.tsx
+++ app/components/layout/work/Projects.tsx
@@ -42,17 +42,37 @@ const Divider = () => (
   </div>
 );
 
-const ProjectItem = ({ image, title, description }: typeof projectData[0]) => (
-    <div className="flex gap-4">
-      <div className="w-1/2">
-        <img src={image} className="w-full h-auto object-contain" alt={title} />
-      </div>
-      <div className="w-1/2 flex flex-col gap-2 pr-10">
-        <h2 className="text-3xl font-medium m-0 leading-none">{title}</h2>
-        <p className="font-extralight text-xl">{description}</p>
+const ProjectItem = ({
+  image,
+  title,
+  description,
+}: (typeof projectData)[0]) => (
+  <div className="flex gap-4">
+    {/* Image Container */}
+    <div className="w-1/2 relative group overflow-hidden">
+      <img src={image} className="w-full h-auto object-contain" alt={title} />
+
+      {/* Custom Gradient Overlay */}
+      <div
+        className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end justify-center"
+        style={{
+          background:
+            "linear-gradient(to top, rgba(0,0,0,0.8) 0%, rgba(0,0,0,0.5) 25%, rgba(0,0,0,0) 65%)",
+        }}
+      >
+        <p className="text-white text-2xl font-extralight mb-16 hover:cursor-pointer hover:font-light duration-200">
+          View More
+        </p>
       </div>
     </div>
-  );
+
+    {/* Text Content */}
+    <div className="w-1/2 flex flex-col gap-2 pr-10">
+      <h2 className="text-3xl font-medium m-0 leading-none">{title}</h2>
+      <p className="font-extralight text-xl">{description}</p>
+    </div>
+  </div>
+);
 
 const Projects = () => {
   return (
