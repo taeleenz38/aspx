02905a2fc605c7e2b2d6dc2da2ffa67c6fc716a5
Tae Kyu Lee <taekyu.lee3@gmail.com>
2025-06-24 07:51:47 +1000

    people - animation | work - animation refactoring

--- app/components/ui/PeopleCarousel.tsx
+++ app/components/ui/PeopleCarousel.tsx
@@ -8,52 +8,67 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 const mockPeople = [
   {
     img: "/mock-portrait-1.png",
-    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
   },
   {
     img: "/mock-portrait-3.webp",
-    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
   },
   {
     img: "/mock-portrait-7.jpeg",
-    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
   },
   {
     img: "/mock-portrait-8.jpg",
-    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
   },
   {
     img: "/mock-portrait-9.jpg",
-    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
   },
 ];
 
 const PeopleCarousel = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, margin: "0px 0px -50px 0px" });
+
   return (
-    <Carousel className="w-full h-164 hover:cursor-grab">
+    <Carousel className="w-full h-110 hover:cursor-grab" ref={ref}>
       <CarouselContent className="-ml-4 h-full">
         {mockPeople.map((person, index) => (
           <CarouselItem
             key={index}
             className="lg:basis-1/2 xl:basis-1/3 h-full"
           >
-            <div className="w-full h-164 overflow-hidden mb-4">
-              <img
-                src={person.img}
-                alt={`Portrait ${index + 1}`}
-                className="w-full h-full object-cover rounded-xl shadow-lg"
-              />
-            </div>
-            <p className="text-md font-extralight text-center px-8">{person.desc}</p>
+            <motion.div
+              initial={{ opacity: 0, y: 50 }}
+              animate={isInView ? { opacity: 1, y: 0 } : {}}
+              transition={{ duration: 0.4, delay: index * 0.2 }}
+              className="w-full"
+            >
+              <div className="h-110 overflow-hidden mb-4 rounded-xl shadow">
+                <img
+                  src={person.img}
+                  alt={`Portrait ${index + 1}`}
+                  className="w-full h-full object-cover rounded-xl"
+                />
+              </div>
+              <p className="text-lg font-extralight text-center px-4 mt-4">
+                {person.desc}
+              </p>
+            </motion.div>
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious className="hover:cursor-pointer" />
-      <CarouselNext className="hover:cursor-pointer" />
+
+      <CarouselPrevious className="hover:cursor-pointer hover:bg-black hover:text-white" />
+      <CarouselNext className="hover:cursor-pointer hover:bg-black hover:text-white" />
     </Carousel>
   );
 };
