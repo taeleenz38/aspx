e7b29fa3a311988d3cc07e62c4ff16c2c669b79c
Tae Lee <104185523+taeleenz38@users.noreply.github.com>
2025-07-15 18:38:15 +1000

    Update Projects.tsx

--- app/components/layout/work/Projects.tsx
+++ app/components/layout/work/Projects.tsx
@@ -1,54 +1,9 @@
 "use client";
 
-
 import React, { useState } from "react";
-
 import { motion as MotionComponent } from "framer-motion";
 import ProjectsCarousel from "../../ui/ProjectsCarousel";
-const projectData = [
-  {
-    category: "videographers",
-    thumbnail: "/mock-project-3.jpg",
-    images: [
-      "/mock-project-1.jpg",
-      "/mock-project-1.jpg",
-      "/mock-project-1.jpg",
-      "/mock-project-1.jpg",
-      "/mock-project-1.jpg",
-      "/mock-project-1.jpg",
-    ],
-    title: "Title 1",
-    description: "Sample description for videographers...",
-  },
-  {
-    category: "photographers",
-    thumbnail: "/mock-project-2.png",
-    images: [
-      "/mock-project-1.jpg",
-      "/mock-project-1.jpg",
-      "/mock-project-1.jpg",
-      "/mock-project-1.jpg",
-      "/mock-project-1.jpg",
-      "/mock-project-1.jpg",
-    ],
-    title: "Title 2",
-    description: "Sample description for photographers...",
-  },
-  {
-    category: "artists",
-    thumbnail: "/mock-project-1.jpg",
-    images: [
-      "/mock-project-1.jpg",
-      "/mock-project-1.jpg",
-      "/mock-project-1.jpg",
-      "/mock-project-1.jpg",
-      "/mock-project-1.jpg",
-      "/mock-project-1.jpg",
-    ],
-    title: "Title 3",
-    description: "Sample description for artists...",
-  },
-];
+import projectData from "../../data/ProjectsData";
 
 const Divider = () => (
   <div className="w-1/2 flex justify-center">
